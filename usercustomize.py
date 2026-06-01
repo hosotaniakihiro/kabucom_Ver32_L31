5fe5a045ba7565cf54ef6f4d2db3324868dd1067
hosotaniakihiro <145765536+hosotaniakihiro@users.noreply.github.com>
2026-06-01 14:45:17 +0900

    Auto install TONOSAMA failopen direction rescue

--- usercustomize.py
+++ usercustomize.py
@@ -15,3 +15,4 @@ _install("TONOSAMA_LUNCH_REOPEN_RECENT", "core.startup.tonosama_lunch_reopen_rec
 _install("YAHOO_COMPUTE_SCHEMA_NA_GUARD", "core.startup.yahoo_compute_schema_na_guard_patch")
 _install("RANKING_ENTRY_FAST_BUDGET_OVERRIDE", "core.startup.ranking_entry_fast_budget_override_patch")
 _install("TONOSAMA_RECENT_3M5M_FAILOPEN", "core.startup.tonosama_recent3m5m_failopen_patch")
+_install("TONOSAMA_FAILOPEN_DIRECTION_RESCUE", "core.startup.tonosama_failopen_direction_rescue_patch")
