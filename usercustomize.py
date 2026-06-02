bd90fe832341158ecb9ce4dc6ba8d4c31aa77f7a
hosotaniakihiro <145765536+hosotaniakihiro@users.noreply.github.com>
2026-06-02 09:15:57 +0900

    Auto install watchlist liquidity empty fail-open patch

--- usercustomize.py
+++ usercustomize.py
@@ -16,3 +16,4 @@ _install("YAHOO_COMPUTE_SCHEMA_NA_GUARD", "core.startup.yahoo_compute_schema_na_
 _install("RANKING_ENTRY_FAST_BUDGET_OVERRIDE", "core.startup.ranking_entry_fast_budget_override_patch")
 _install("TONOSAMA_RECENT_3M5M_FAILOPEN", "core.startup.tonosama_recent3m5m_failopen_patch")
 _install("TONOSAMA_FAILOPEN_DIRECTION_RESCUE", "core.startup.tonosama_failopen_direction_rescue_patch")
+_install("WATCHLIST_LIQ_EMPTY_FAILOPEN", "core.startup.watchlist_liq_empty_failopen_register_patch")
