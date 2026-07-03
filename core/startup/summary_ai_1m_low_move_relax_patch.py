f4d144e63675f8b7cd32fbd32458884e7fd7d966
hosotaniakihiro <145765536+hosotaniakihiro@users.noreply.github.com>
2026-07-03 10:47:54 +0900

    Make low move stub delegate to range relax patch

--- core/startup/summary_ai_1m_low_move_relax_patch.py
+++ core/startup/summary_ai_1m_low_move_relax_patch.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-# touch
+from core.startup.summary_ai_1m_range_relax_patch import VERSION, install
+
+try:
+    install()
+except Exception:
+    pass
+
+__all__ = ["VERSION", "install"]
